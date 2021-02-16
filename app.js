7cfb9672d8ccd072056e1f945d8d6db8d35c5200
Christina Loiacono <loiaconochristina@gmail.com>
2021-02-16 12:19:32 -0500

    Added Back to Top button

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+// This inserts the function into the "onclick" event for the button.
 function myFunction() {
-    document.getElementById("normal-stuff").innerHTML = "Paragraph changed because you clicked the button.";
+    document.getElementById("normal-lorum-ipsum").innerHTML = "Paragraph changed because you clicked the button.";
   }
 
+
+// This creates a "back to top" button, scrolling up to (0,0) coordinates.
+document.getElementById('btnScrollTop')
+  .addEventListener('click', function(){
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+
+  });
